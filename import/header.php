8c4fd11c05e66f73614add17efde20ddf527ba9b
Gamenight77 <game.night.77176@gmail.com>
2024-05-16 12:08:49 +0200

    Premier commit de la SAE

---
+++ import/header.php
@@ -0,0 +1,22 @@
+
+<header>
+<nav class="navbar">
+    <div class="container">
+        <div class="brand">
+            <a href="#">Jeux Olympiques 2024</a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Événements</a></li>
+            <li><a href="#">Résultats</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="auth-buttons">
+            <a href="" class="btn-login">Connexion</a>
+            <a href="" class="btn-signup">Inscription</a>
+        </div>
+    </div>
+</nav>
+</header>
+
+
